59456837192a788d19950810050afebe62a486b4
Sunitha Iyadurai <sunithaiyadurai@gmail.com>
2021-03-23 00:39:29 +0000

    JavaScript Studio 10.11 Completed

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 //We want to COMPLETELY reverse an array by flipping the order of the entries AND flipping the order of characters in each element.
 
 //1) a) Define a 'reverseCharacters' function. Give it one parameter, which will be the string to reverse.
+
 //b) Within the function, split the string into an array, then reverse the array.
 //c) Use join to create the reversed string and return that string from the function.
 //d) Below the function, define and initialize a variable to hold a string.
 //e) Use console.log(reverseCharacters(myVariableName)); to call the function and verify that it correctly reverses the characters in the string.
 //f) Use method chaining to reduce the lines of code within the function.
-
-
+console.log("Part 1");
+function reverseCharacters(str) {
+  let lettersArray = str.split('');
+  console.log(lettersArray);
+  let reversedLettersArray = lettersArray.reverse();
+  return reversedLettersArray.join('');
+}
+let myVariableName = "Capitalized Letters";
+console.log(reverseCharacters(myVariableName));
 
 //2) The 'split' method does not work on numbers, but we want the function to return a number with all the digits reversed (e.g. 1234 converts to 4321 and NOT the string "4321")
 //a) Add an if statement to your reverseCharacters function to check the typeof the parameter.
 //b - d) If typeof is ‘string’, return the reversed string as before. If typeof is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
 //e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next exercise.
-
+console.log("Part 2")
+function reverseCharacters(num) {
+  if(typeof num==='number'){
+    
+  num=String(num);
+  return Number(num.split("").reverse().join(""))
+  }else{
+    return num.split("").reverse().join("");
+  }
+    }
+reverseCharacters(1234);
 //3) Create a new function with one parameter, which is the array we want to change. The function should:
 //a) Define and initialize an empty array.
 //b) Loop through the old array.
@@ -21,6 +39,20 @@
 //d) Add the reversed string (or number) to the array defined in part ‘a’.
 //e) Return the final, reversed array.
 //f) Be sure to print the results from each test case in order to verify your code.
+ console.log("Part 3")
+ function reverseArray(array) {
+ let result=[];
+ for(let i=0;i<array.length;i++)
+ {
+   result.unshift(reverseCharacters(array[i]));
+   console.log(result);
+ } 
+ return result;
+ }
+ reverseArray(['hello', 'world', 123, 'orange']);
+
+// let myVariableArray = ['hello', 'world', 123, 'orange'];
+// console.log(reverseArray(myVariableArray));
 
 
 
